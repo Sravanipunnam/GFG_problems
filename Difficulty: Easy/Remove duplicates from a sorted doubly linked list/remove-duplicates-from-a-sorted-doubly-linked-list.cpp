37e6bd53cc1c604ad17258c6ef33a98c0e84617c
Sravanipunnam <147476006+Sravanipunnam@users.noreply.github.com>
2025-09-20 16:23:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove duplicates from a sorted doubly linked list/remove-duplicates-from-a-sorted-doubly-linked-list.cpp
@@ -0,0 +1,38 @@
+/*
+struct Node
+{
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+
+};
+*/
+
+class Solution {
+  public:
+
+    Node *removeDuplicates(struct Node *head) {
+        // Your code here
+        Node* temp=head;
+        while(temp!=nullptr && temp->next!=nullptr)
+        {
+            Node *nextNode=temp->next;
+            while(nextNode !=nullptr && nextNode->data==temp->data)
+            {
+                Node * todel=nextNode;
+                nextNode=nextNode->next;
+                delete (todel);
+            }
+            temp->next=nextNode;
+            if(nextNode) nextNode->prev=temp;
+            temp=temp->next;
+        }
+        return head;
+    }
+};
