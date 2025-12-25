19ad9641b146765cde460d8f4cf223ca2279a172
Sravanipunnam <147476006+Sravanipunnam@users.noreply.github.com>
2025-12-25 09:36:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find the Peak Element in a 2D Matrix/find-the-peak-element-in-a-2d-matrix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                int curr = mat[i][j];
+
+                int top    = (i > 0)     ? mat[i-1][j] : -1e9;
+                int bottom = (i < n-1)   ? mat[i+1][j] : -1e9;
+                int left   = (j > 0)     ? mat[i][j-1] : -1e9;
+                int right  = (j < m-1)   ? mat[i][j+1] : -1e9;
+
+                if (curr >= top && curr >= bottom &&
+                    curr >= left && curr >= right) {
+                    return {i, j};
+                }
+            }
+        }
+        return {-1, -1}; // safety
+    }
+};
